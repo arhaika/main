89b9381c1adda493ff59cefde037f1a91aa7e4c4
arhaika <arhaik1@yandex.ru>
2024-03-26 18:14:06 +0300

    JS lvl1
    JS lvl1

---
+++ Управляющие конструкции/script.js
@@ -0,0 +1,46 @@
+
+/*age = 12
+
+
+console.log(age) 
+let huy = 30
+if (huy === 30)
+{console.log('u have a greate dick')}
+
+if (age >= 31) {
+    console.log( 'starper');
+}
+
+
+let isRainyNow = true;
+
+if(isRainyNow === true) 
+{console.log('беру зонт');}
+
+let children = 3;
+if (children >0)
+{console.log('go to zoo');}
+*/
+let age = 32
+if (age >= 31) {
+    console.log( 'boomer');}
+    else if (age<31)
+    {console.log('zoomer')}
+
+    age >=31 ? console.log( 'boomer') : console.log('zoomer') //тернарный оператор
+
+//циклы
+
+let num = 0;
+while (num<5)
+{console.log(num);
+//num = num+1
+//num +=1
+num++ }//увеличивает зрначение на единицу любой из вариантов
+console.log ('конец цикла');
+
+//for (let num = 1; num <= 5; num++ )
+//{console.log(num);}
+
+for (let num = 0; num <= 20; num+= 2 )
+{console.log(num);}
