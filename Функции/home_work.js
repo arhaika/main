ac2a2efd7b2786bb2dc87d9222c8564408a41b0d
arhaika <arhaik1@yandex.ru>
2024-03-28 16:34:19 +0300

    1
    1

---
+++ Функции/home_work.js
@@ -0,0 +1,25 @@
+//1 задание
+function welcomeUser (welcome, userName) {
+    console.log(welcome + userName);
+}
+let welcome = 'Шалом, дорогой товарищ'
+let fName = ' Висусуолий'
+welcomeUser (welcome, fName)
+
+//2 задание
+function sProd (num1, num2, num3) {
+   console.log((num1 + num2)* num3);
+}
+sProd (1, 3, 6)
+
+//3
+function Multy (mass, num) {
+    
+    for (let i =0; i < mass.length; i++){
+    console.log (mass[i] * num);}
+}
+Multy ([1,2,3,4,5],2)
+
+
+
+//4
--- Функции/script.js
+++ Функции/script.js
@@ -1,53 +1,191 @@
-//объявление функции
-/*function SayHello () {
-    console.log('добрейший вечерочек');
-}
+// 1 объявление функции
+// function sayHi() {
+//   console.log("Hi");
+// }
 
-//вызов функции
-SayHello();*/
-
-/*function getSumm(num1, num2) //num1,2 - параметры функции
- {
-    console.log(num1 + num2);
-}
-getSumm(2, 2);
-getSumm(5, 7); */
-
-//получение суммы элементов массива
-/*function getArraySum (arrayNum) {
-    let sum = 0
-    for (let num of arrayNum)
-{sum +=num;}
-console.log(sum);
-}
-
-let nums =[3,5,7,8,2,3]
-let bills =[234,567,890, 247, 947]
-getArraySum (nums)*/
+// 2 вызов функции
+// sayHi();
+// sayHi();
+// sayHi();
+// sayHi();
 
 /*
-function getPow(num, pow) {
-    let result = num ** pow
-    console.log(result);
-}
-getPow (10) 
+let userName = "Иван";
+console.log(userName);
+console.log(userName.length); // получаем длину строки
+console.log(userName[0]);
 */
-/*
-//возврат значения из функции
-//в теле считаем функцию и возвращаем
-function getPow(num, pow) {
-    let result = num ** pow
-    return result;
-}
-//работаем со значением за пределами функции
-let numPow = getPow(10,2)
-console.log(numPow);*/
 
-function doMatch(num1, num2) {
-    let sum = num1 + num2;
-    let prod = num1 * num2;
-    return {summa: sum, product: prod} //  в {} содержится объект. в массиве могут быть объекты и наоборот
-    
+/*
+function getSum() {
+  console.log(2 + 2);
 }
-let myMatch = doMatch(5,5)
-console.log(myMatch);
+getSum();
+getSum();
+getSum();
+getSum();
+*/
+
+// num1, num2 - параметры функции
+/*
+function getSum(num1, num2) {
+  console.log(num1 + num2);
+}
+
+getSum(2, 2);
+getSum(5, 5);
+getSum(43, 78);
+*/
+
+/*
+let age = "hello";
+console.log(typeof age);
+*/
+
+/*
+// функция для получения суммы элементов массива
+function getArraySum(arrayNum) {
+  let sum = 0;
+  for (let num of arrayNum) {
+    sum += num;
+  }
+  //console.log(typeof sum);
+  if (typeof sum === "number") {
+    console.log(sum);
+  } else {
+    console.log("Внимание! В массиве есть элемент не числового типа'!!");
+  }
+}
+
+let nums = [3, 5, 6, 8, 2, 3];
+let bills = [234, 645, 234, 767, 234];
+
+getArraySum(nums);
+getArraySum(bills);
+*/
+
+/*
+function getProduct(num1, num2) {
+  let result = num1 * num2; // num1 = 10, num2 = undefined; 10 + undef
+  console.log(result);
+}
+
+getProduct(10);
+*/
+
+/*
+function getPow(num, pow = 1) {
+  let result = num ** pow;
+  console.log(result);
+}
+
+getPow(10);
+getPow(10, 5);
+getPow(32);
+*/
+
+/*
+function getPow(pow = 1, num) {
+  let result = num ** pow;
+  console.log(result);
+}
+
+getPow(10);
+*/
+
+/**
+ * возврат значения из функции
+ */
+/*
+function getPow(num, pow) {
+  let result = num ** pow;
+}
+
+getPow(10, 2);
+getPow(2, 2);
+
+console.log(result);*/
+
+/*
+function getPow(num, pow) {
+  let result = num ** pow;
+  return result;
+}
+
+let numPow = getPow(10, 2);
+console.log(numPow);
+console.log(numPow * 5);
+*/
+
+/*
+// посчитать сумму и произведение двух чисел
+function doMath(num1, num2) {
+  let sum = num1 + num2;
+  let prod = num1 * num2;
+
+  return [sum, prod];
+}
+
+let myMath = doMath(5, 5);
+console.log(myMath);
+*/
+
+/*
+// посчитать сумму и произведение двух чисел
+function doMath(num1, num2) {
+  let sum = num1 + num2;
+  let prod = num1 * num2;
+
+  // возвращаем ОБЪЕКТ
+  return { summa: sum, product: prod };
+}
+
+let myMath = doMath(5, 5);
+console.log(myMath);
+
+//let student = ["Иван", "Иванов", 22, 2, true];
+// объект
+let student = {
+  fName: "Иван",
+  lName: "Иванов",
+  age: 22,
+  cource: 2,
+  isAdmin: true,
+};
+*/
+
+/*
+// массив объектов
+let cart = [
+  {
+    title: "Пианино",
+    price: 321,
+    qty: 3,
+  },
+  {
+    title: "Дудка",
+    price: 123,
+    qty: 6,
+  },
+  {
+    title: "Флейта",
+    price: 654,
+    qty: 1,
+  },
+];
+console.log(cart);
+*/
+
+/*
+let student = {
+  fName: "Иван",
+  lName: "Иванов",
+  age: 22,
+  cource: 2,
+  isAdmin: true,
+};
+
+console.log(student);
+console.log(student.fName);
+console.log(student.lName);
+*/
